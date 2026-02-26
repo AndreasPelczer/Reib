eadf645f9c981333035937d49c8fd2064a9c8573
Andreas Pelczer <134264285+AndreasPelczer@users.noreply.github.com>
2026-02-26 12:54:56 +0100

    Add files via upload

---
+++ Reib/ReibTests/ReibTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReibTests.swift
+//  ReibTests
+//
+//  Created by Andreas Pelczer on 26.02.26.
+//
+
+import Testing
+@testable import Reib
+
+struct ReibTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Reib/ReibUITests/ReibUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ReibUITestsLaunchTests.swift
+//  ReibUITests
+//
+//  Created by Andreas Pelczer on 26.02.26.
+//
+
+import XCTest
+
+final class ReibUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
